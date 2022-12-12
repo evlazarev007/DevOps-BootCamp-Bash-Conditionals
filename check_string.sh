6139188075b4ef78afa1cbf3649639c685de0291
evlazarev007 <115842128+evlazarev007@users.noreply.github.com>
2022-12-13 02:29:40 +0600

    add code

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-# Place your code here
+input="$1"
+output=""
+numbers=0
+symbols=0
+letters=0
+
+for ((i=0;i<=${#input};i++)); do
+	char="${input:$i:1}"
+	if [[ $char =~ [0-9] ]]; then
+		numbers=$(($numbers+1))
+	elif [[ $char =~ [A-Za-z] ]]; then
+		letters=$(($letters+1))
+	elif [[  $char =~ [^A-Za-z0-9\ ] ]]; then
+		symbols=$(($symbols+1))
+	fi
+done
+
+echo "Nubers: $numbers Symbols: $symbols Letters: $letters"
