d27f6113f2b87be8317e927f931b4c412656606a
evlazarev007 <115842128+evlazarev007@users.noreply.github.com>
2022-12-13 02:39:45 +0600

    11

--- cond1.sh
+++ cond1.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-input="$1"
-sum=0
+IFS=',' read -r -a array <<< "$1"
 
-for ((i=0;i<=${#input};i++)); do
-	char="${input:$i:1}"
-	if [[ $char =~ [0-9] ]]; then
-		a=$(($char%2))
-		if [[ $a -eq 0 ]]; then
-			sum=$(($sum+$char))
-		fi
-	fi
 
-done
 
-echo "$sum"
+#Write your code here
