e2c599c328e735eb84414a32f765c69ae86bee98
evlazarev007 <115842128+evlazarev007@users.noreply.github.com>
2022-12-13 02:58:45 +0600

    add code

--- cond1.sh
+++ cond1.sh
@@ -4,4 +4,13 @@ IFS=',' read -r -a array <<< "$1"
 
 
 
-#Write your code here
+sum=0
+
+for ((i=0;i<${#array[@]};i++)); do
+	a=$((${array[i]}%2))
+	if [[ $a -eq 0 ]]; then
+		sum=$(($sum+${array[i]}))
+	fi
+done
+
+echo "$sum"
