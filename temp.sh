9706dd1f7b06ff6a791ba3d617d3c17380eed85c
evlazarev007 <115842128+evlazarev007@users.noreply.github.com>
2022-12-13 02:28:59 +0600

    add code

--- temp.sh
+++ temp.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-# Place your code here
+input="$1"
+output=0
+len=${#input}
+
+	char="${input:$len-1:1}"
+	if [[ $char = "C" ]]; then
+		t=${input/C/}
+		output=$(($t+273))K
+	elif [[ $char = "K" ]]; then
+		t=${input/K/}
+		output=$(($t-273))C
+	fi
+
+
+echo "$output"
